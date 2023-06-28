1a09ad195d9ce5777107e13507aee5d21e3c9627
AditiSharma <aditi2003sharma@gmail.com>
2023-06-29 01:38:22 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b330d0a90236eb9b3ace11cad28e24e0",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
